1ab6939bb9f681e5d45b54a75abccdb6f1131617
Azamat <azamatduishenkulov711@gmail.com>
2022-06-22 15:16:10 +0600

    add adaptive  about

--- components/About/About.jsx
+++ components/About/About.jsx
@@ -5,16 +5,19 @@ export default function About() {
     return (
         <header className={styles.about}>
             <div className={styles.container}>
-                <div className={styles.info}>
-                    <div className={styles.titles}>
-                    <h2 className={styles.about__h2}>О Нас</h2>
-                    <h2 className={styles.about__description}>Мы подберем вам уникальные дизайны</h2>
+                <div className={styles.block}>
+                    <div className={styles.info}>
+                        <div className={styles.titles}>
+                        <h2 className={styles.about__h2}>О Нас</h2>
+                        <h2 className={styles.about__description}>Мы подберем вам уникальные дизайны</h2>
+                        </div>
+                        <a href="#" className={styles.about__link}>Узнать еще</a>
                     </div>
-                    <a href="#" className={styles.about__link}>Узнать еще</a>
+                    <div className={styles.img}>
+                        <img src="images/design.png" alt="" className={styles.img__about} />
+                    </div>
                 </div>
-                <div className={styles.img}>
-                    <img src="images/design.png" alt="" className={styles.img__about} />
-                </div>
+            
             </div>
         </header>
     )
