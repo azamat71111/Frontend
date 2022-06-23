37eeda11f519bd41d92f0e91211793131e1de1e9
Azamat <azamatduishenkulov711@gmail.com>
2022-06-23 12:35:00 +0600

    add adaptive services

--- components/Services/Services.jsx
+++ components/Services/Services.jsx
@@ -4,14 +4,14 @@ import styles from "./Services.module.sass"
 export default function Services() {
 return (
     <div className={styles.container}>
-        <div className={styles.card__1}>
+        <div className={`${styles.card} ${styles.card__1}`}>
             <div className={styles.info__service}>
-                <h4 className={styles.h4__service}>Начало</h4>
+                <h4 className={`${styles.h4__service} ${styles.h4__service1}`}>Начало</h4>
                 <p className={styles.p__info}>Мы подберем вам дизайн  Рассчитаем расходы  Сделаем ремонт помещения Дадим ключи в ваши руки</p>
             </div>
             <img src="images/start.png" alt="" className={styles.img__service} />
         </div>
-        <div className={`${styles.card__1} ${styles.card__2}`}>
+        <div className={`${styles.card} ${styles.card__2}`}>
         <img src="images/assessment.png" alt="" className={`${styles.img__service} ${styles.img__service2}`} />
             <div className={`${styles.info__service} ${styles.info__service2}`}>
                 <h4 className={`${styles.h4__service} ${styles.h4__service2}`}>Оценка</h4>
