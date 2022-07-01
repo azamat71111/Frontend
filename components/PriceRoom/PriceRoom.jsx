862271ec90376be29ad0f7019528889e48c2ba0d
Erbol <a1darbekov640@gmail.com>
2022-07-01 11:50:12 +0300

    Add heroku

--- components/PriceRoom/PriceRoom.jsx
+++ components/PriceRoom/PriceRoom.jsx
@@ -97,6 +97,8 @@ export default function PriceRoom() {
             />
             <button className={styles.form__btn}>Отправить</button>
           </form>
+          {loading === 'complete' ? <h3 className={styles.complete}>Успешно!</h3> : null}
+          {loading === 'error' ? <h3 className={styles.error}>Сообщения уже отправлено!</h3> : null}
         </div>
       </header>
     </div>
