67177bb2246fed6c8752dfcb0660ac31efa22e8d
Melis <tyn.melis.ru@gmail.com>
2022-07-02 14:59:46 +0300

    add func

--- components/MainHead/MainHead.jsx
+++ components/MainHead/MainHead.jsx
@@ -1,18 +1,44 @@
 import React from "react";
 import styles from "./MainHead.module.sass";
-export default function MainHead() {
+import { motion } from "framer-motion";
+
+export default function MainHead({ width }) {
+  const variantImg = {};
+  // if (width) {
+  //   variantImg = {
+  //     hidden: {
+  //       x: "20px",
+  //     },
+  //     animate: {
+  //       x: "0",
+  //       transition: {
+  //         type: "spring",
+  //         stiffness: 400,
+  //         damping: 20,
+  //         duration: 0.3,
+  //       },
+  //     },
+  //   };
+  // } else {
+  //   return;
+  // }
   return (
     <header className={styles.head}>
       <main className={styles.container}>
         <div className={styles.main__content}>
-          <div className={styles.main__img}>
+          <motion.div
+            initial="hidden"
+            animate="animate"
+            variants={variantImg}
+            className={styles.main__img}
+          >
             <img src="/images/mainPhoto.png" alt="Eurasia photo" />
             <div className={styles.main__cart}>
               <span className={styles.new__text}>NEW</span>
               <h4 className={styles.cart__title}>Новые квартиры</h4>
               <h4 className={styles.cartSub__title}>По новым стандартам</h4>
             </div>
-          </div>
+          </motion.div>
         </div>
       </main>
     </header>
