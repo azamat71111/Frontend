222286e1e2893ff31d79b55f522c922b345c86c8
Erbol <a1darbekov640@gmail.com>
2022-07-04 11:59:11 +0300

    add animation framer to Upper and Realters

--- components/Realters/Realters.jsx
+++ components/Realters/Realters.jsx
@@ -1,6 +1,64 @@
 import axios from "../../api/axios";
 import React, { useEffect, useState } from "react";
 import styles from "./Realters.module.sass";
+import { motion } from "framer-motion";
+
+let firstVar = {
+  hidden: {
+    x: -20,
+    opacity: 0,
+  },
+  visible: (num) => ({
+    x: 0,
+    opacity: 1,
+    transition: {
+      duration: 0.5,
+      delay: num * 0.1,
+    },
+  }),
+};
+let secondVar = {
+  hidden: {
+    x: 40,
+    opacity: 0,
+  },
+  visible: (num) => ({
+    x: 0,
+    opacity: 1,
+    transition: {
+      duration: 0.5,
+      delay: num * 0.1,
+    },
+  }),
+};
+let bottomVar = {
+  hidden: {
+    y: 40,
+    opacity: 0,
+  },
+  visible: (num) => ({
+    y: 0,
+    opacity: 1,
+    transition: {
+      duration: 0.5,
+      delay: num * 0.1,
+    },
+  }),
+};
+let topVar = {
+  hidden: {
+    y: -40,
+    opacity: 0,
+  },
+  visible: (num) => ({
+    y: 0,
+    opacity: 1,
+    transition: {
+      duration: 0.5,
+      delay: num * 0.1,
+    },
+  }),
+};
 
 export default function Realters() {
   const [user, setUser] = useState({
@@ -44,12 +102,42 @@ export default function Realters() {
   return (
     <>
       <div className={styles.container}>
-        <h4 className={styles.title}>НУЖНЫ РЕАЛТЕРЫ</h4>
-        <h2 className={styles.title2}>
+        <motion.h4
+          initial="hidden"
+          whileInView="visible"
+          viewport={{
+            once: true,
+            amount: 0.3,
+          }}
+          variants={bottomVar}
+          custom={0}
+          className={styles.title}
+        >
+          НУЖНЫ РЕАЛТЕРЫ
+        </motion.h4>
+        <motion.h2
+          initial="hidden"
+          whileInView="visible"
+          viewport={{
+            once: true,
+            amount: 0.3,
+          }}
+          variants={topVar}
+          custom={0}
+          className={styles.title2}
+        >
           Если вам нужно продать свою квартиру, заполните поле
-        </h2>
+        </motion.h2>
         <form onSubmit={submitHandler} className={styles.input}>
-          <input
+          <motion.input
+            initial="hidden"
+            whileInView="visible"
+            viewport={{
+              once: true,
+              amount: 0.3,
+            }}
+            variants={secondVar}
+            custom={0}
             onChange={onChange}
             value={user.name}
             name="name"
@@ -57,7 +145,15 @@ export default function Realters() {
             placeholder={error ? "Заполните поля!" : "Ваше имя"}
             className={error ? styles.errorName : styles.input__name}
           />
-          <input
+          <motion.input
+            initial="hidden"
+            whileInView="visible"
+            viewport={{
+              once: true,
+              amount: 0.3,
+            }}
+            variants={firstVar}
+            custom={0}
             onChange={onChange}
             value={user.phone}
             name="phone"
@@ -65,7 +161,19 @@ export default function Realters() {
             placeholder={error ? "Заполните поля!" : "Ваш номер"}
             className={error ? styles.errorName : styles.input__email}
           />
-          <button className={styles.btn}>Отправить</button>
+          <motion.button
+            initial="hidden"
+            whileInView="visible"
+            viewport={{
+              once: true,
+              amount: 0.3,
+            }}
+            variants={bottomVar}
+            custom={0}
+            className={styles.btn}
+          >
+            Отправить
+          </motion.button>
         </form>
         {loading === "complete" ? (
           <h3 className={styles.complete}>Успешно!</h3>
--- components/Upper/Upper.jsx
+++ components/Upper/Upper.jsx
@@ -1,26 +1,114 @@
 import React from "react";
 import styles from "./Upper.module.sass";
 import Link from "next/link";
+import { motion } from "framer-motion";
+
+let leftVar = {
+  hidden: {
+    x: -200,
+    opacity: 0,
+  },
+  visible: (num) => ({
+    x: 0,
+    opacity: 1,
+    transition: {
+      duration: 0.5,
+      delay: num * 0.1,
+    },
+  }),
+};
+let rightVar = {
+  hidden: {
+    x: 200,
+    opacity: 0,
+  },
+  visible: (num) => ({
+    x: 0,
+    opacity: 1,
+    transition: {
+      duration: 0.5,
+      delay: num * 0.1,
+    },
+  }),
+};
+let bottomVar = {
+  hidden: {
+    y: 40,
+    opacity: 0,
+  },
+  visible: (num) => ({
+    y: 0,
+    opacity: 1,
+    transition: {
+      duration: 0.5,
+      delay: num * 0.1,
+    },
+  }),
+};
+let topVar = {
+  hidden: {
+    y: -40,
+    opacity: 0,
+  },
+  visible: (num) => ({
+    y: 0,
+    opacity: 1,
+    transition: {
+      duration: 0.5,
+      delay: num * 0.1,
+    },
+  }),
+};
 
 function Upper() {
   return (
     <div className={styles.upper}>
       <div className={styles.upper_container}>
         <div className={styles.grid}>
-          <div className={styles.grid_block}>
+          <motion.div
+            initial="hidden"
+            whileInView="visible"
+            viewport={{
+              once: true,
+              amount: 0.3,
+            }}
+            variants={rightVar}
+            custom={0}
+            className={styles.grid_block}
+          >
             <img src="/images/upper-1.svg" />
             <h2>Продать</h2>
-          </div>
+          </motion.div>
 
-          <div className={styles.grid_block}>
+          <motion.div
+            initial="hidden"
+            whileInView="visible"
+            viewport={{
+              once: true,
+              amount: 0.3,
+            }}
+            variants={topVar}
+            custom={0}
+            className={styles.grid_block}
+          >
             <img src="/images/upper-2.svg" />
             <h2>Купить</h2>
-          </div>
+          </motion.div>
 
-          <div className={styles.grid_block}>
+          <motion.div
+            initial="hidden"
+            whileInView="visible"
+            viewport={{
+              once: true,
+              amount: 0.3,
+            }}
+            variants={leftVar}
+            custom={0}
+            className={styles.grid_block}
+          >
             <img src="/images/upper-3.svg" />
             <h2>Оценка</h2>
-          </div>
+          </motion.div>
         </div>
       </div>
     </div>
