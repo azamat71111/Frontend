34b194da2378ca43f58fa803eeca13376d277fa5
Erbol <a1darbekov640@gmail.com>
2022-07-06 18:19:04 +0300

    add reater

--- components/Realters/Realters.jsx
+++ components/Realters/Realters.jsx
@@ -184,7 +184,7 @@ export default function Realters({ width }) {
           custom={1}
           className={styles.title}
         >
-          НУЖНЫ РЕАЛТЕРЫ
+          НУЖНЫ РИЭЛТОРА?
         </motion.h4>
         <motion.h2
           initial="hidden"
